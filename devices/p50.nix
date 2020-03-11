030a2d0ea29ca7a7eeb7878e7198a8260216db17
Alex Eyre <alex@turm.pw>
2020-03-11 18:43:59 +0000

    Laptop changes

--- devices/p50.nix
+++ devices/p50.nix
@@ -6,17 +6,9 @@
 
 {
 	hardware.bluetooth.enable = true;
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.efi.efiSysMountPoint = "/boot/efi";
-	boot.loader.grub = {
-		enable = true;
-		device = "nodev";
-		version = 2;
-		efiSupport = true;
-		enableCryptodisk = true;
-		extraInitrd = "/boot/initrd.keys.gz";
-	};
+	hardware.pulseaudio.enable = true;
+	nixpkgs.config.pulseaudio = true;
+	environment.systemPackages = with pkgs; [ pavucontrol ];
 	networking.hostName = "memepad";
 	hardware.bumblebee = {
 		enable = true;
@@ -24,46 +16,61 @@
 		pmMethod = "bbswitch";
 		driver = "nouveau";
 	};
-	imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>];
+
 
 	boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
 	boot.initrd.kernelModules = [ "dm-snapshot" ];
 	boot.kernelModules = [ "kvm-intel" ];
 	boot.extraModulePackages = [ ];
 
-	nix.maxJobs = lib.mkDefault 8;
-	powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-
-
 	fileSystems."/" =
-	{ device = "/dev/disk/by-uuid/28cbad6c-02c5-4637-a26e-813e1357c826";
+	{ device = "/dev/disk/by-uuid/1cc49fed-355d-4190-9cb3-cc63564050b6";
 		fsType = "btrfs";
 	};
 
 	fileSystems."/boot/efi" =
-	{ device = "/dev/disk/by-uuid/DE37-B09F";
+	{ device = "/dev/disk/by-uuid/3040-B4CD";
 		fsType = "vfat";
 	};
+	hardware.enableRedistributableFirmware = true;
 
-	fileSystems."/data" =
-	{ device = "/dev/disk/by-uuid/da208f59-017b-4f0d-a2ea-4fd260cb9bef";
-		fsType = "btrfs";
-		encrypted = {
+	swapDevices =
+		[ { device = "/dev/disk/by-uuid/e71b529b-84fb-416e-b832-093ae522750f"; }
+		];
+
+		nix.maxJobs = lib.mkDefault 8;
+		powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+
+		boot.loader.systemd-boot.enable = true;
+		boot.loader.efi.canTouchEfiVariables = true;
+		boot.loader.efi.efiSysMountPoint = "/boot/efi";
+		boot.loader.grub = {
 			enable = true;
-			label = "crypt-data";
-			blkDev = "/dev/disk/by-uuid/427f80a3-0680-4bde-a559-7a7c86f514b1";
-			keyFile = "/keyfile1.bin";
+			device = "nodev";
+			version = 2;
+			efiSupport = true;
+			enableCryptodisk = true;
+			extraInitrd = /boot/initrd.keys.gz;
 		};
-	};
 
-	boot.initrd.luks.devices = {
+		boot.initrd.luks.devices = {
 		"root" = {
-			name = "root";
-			device = "86c1bccf-5f34-4afe-aea3-2584ef11c3f8";
+			device = "/dev/disk/by-uuid/fee1574b-e92f-4115-9932-9a49a07359ed";
 			preLVM = true;
 			keyFile = "/keyfile0.bin";
 			allowDiscards = true;
 		};
-	};
+		};
 
+# Data mount
+		fileSystems."/data" = {
+			device = "/dev/disk/by-uuid/f00b20fd-daaa-424a-af83-2722da19d773";
+			encrypted = {
+				enable = true;
+				label = "crypted-data";
+				blkDev = "/dev/disk/by-uuid/833196d8-c5a1-4ddc-82f1-d0493936e3f9";
+				keyFile = "/keyfile1.bin";
+			};
+		};
 }
--- main.nix
+++ main.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, mkDerivation, qtbase, qttools, qmake, ... }:
 
 {
 	nixpkgs.config.allowUnfree = true;
@@ -27,12 +27,12 @@ SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", MODE="0666", GROUP="wheel"
 		xorg.xorgserver
 		xorg.xf86videointel
 		gnupg
-		wireguard-tools
 		rockbox-mod
 		p7zip
+		wireguard-tools
 	];
 	virtualisation.virtualbox.host.enable = true;
-	environment.pathsToLink = ["${pkgs.xorg.libxcb}/lib/"];
+	environment.pathsToLink = ["${pkgs.xorg.libxcb}/lib/" "/share/zsh"];
 	services.emacs = {
 		enable = true;
 		package = pkgs.emacsGit;
@@ -40,26 +40,30 @@ SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", MODE="0666", GROUP="wheel"
 		defaultEditor = true;
 	};
 	networking.firewall.enable = true;
+	networking.firewall.checkReversePath = "loose";
+	networking.firewall.allowedUDPPorts = [ 51820 ];
+	networking.wireguard.enable = true;
+	
         services.xserver = {
                 enable = true;
                 layout = "us";
                 xkbVariant = "dvp";
                 xkbOptions = "ctrl:nocaps";
                 libinput.enable = true;
+		libinput.tapping = false;
                 videoDrivers = [ "intel" ];
                 desktopManager = {
                         xterm.enable = false;
                 };
                 windowManager.bspwm.enable = true;
-		desktopManager.plasma5.enable = true;
         };
+	programs.zsh.enable = true;
         users.users.alex = {
                 isNormalUser = true;
                 extraGroups = [ "wheel" "audio" "adbusers" ];
-
+		shell = pkgs.zsh;
         };
 	system.stateVersion = "19.09";
 	services.connman.enable = true;
 	networking.wireless.networks = { TANHETOADETHOADETHAOD = {}; };
-	networking.wireguard.enable = true;
 }
