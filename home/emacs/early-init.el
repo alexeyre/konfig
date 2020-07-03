02fad4c23009c8590c028f0f07987ef4cb93630d
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2020-07-03 10:19:04 +0100

    bro

--- home/emacs/early-init.el
+++ home/emacs/early-init.el
@@ -1,3 +1,24 @@
+(setq gc-cons-threshold 100000000)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+(setq straight-use-symlinks t)
+(setq package-user-dir (expand-file-name "packages" user-emacs-directory))
+(with-no-warnings
+  (require 'cl))
+(setq vc-follow-symlinks t)
+;; bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+;; enable use-package macro
+(straight-use-package 'use-package)
--- home/emacs/init.el
+++ home/emacs/init.el
@@ -1,33 +1,12 @@
-;; -*- lexical-binding: t; -*-
+; -*- lexical-binding: t; -*-
+
 (if (version< emacs-version "27.0")
   (load-file (expand-file-name "early-init.el" user-emacs-directory)))
-(setq gc-cons-threshold 16777216
-      gc-cons-percentage 0.1)
-(setq package-user-dir (expand-file-name "packages" user-emacs-directory))
-(with-no-warnings
-   (require 'cl))
-(setq vc-follow-symlinks t)
-;; bootstrap straight.el
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-;; enable use-package macro
-(straight-use-package 'use-package)
-(straight-use-package 'bind-key)
-(straight-use-package 'diminish)
-;; always straight a package by default
-(setq straight-use-package-by-default t)
-(use-package no-littering
-  :demand t)
-(use-package use-package-ensure-system-package
-  :demand t)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(let ((file-name-handler-alist nil))
+  (straight-use-package 'bind-key)
+  (straight-use-package 'diminish)
+  ;; always straight a package by default
+  (setq straight-use-package-by-default t)
+  (straight-use-package 'no-littering)
+  (straight-use-package 'use-package-ensure-system-package)
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
