a50d21f04cdb07d43026196c2e54813e77ec2a2b
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2020-07-06 10:36:05 +0100

    absolutely degenerate

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -10,3 +10,16 @@
   (straight-use-package 'no-littering)
   (straight-use-package 'use-package-ensure-system-package)
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/notes/20200619184615-index.org" "~/notes/notes.org"))
+ '(safe-local-variable-values '((eval alex/flowy-note) (olivetti-body-width . 110))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
