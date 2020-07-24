19ac19072abe0ee603404680cfc05c09f29ca6fe
Alex Eyre <alex.eyre@cambridgeconsultants.com>
2020-07-24 17:29:18 +0100

    whoop

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -14,8 +14,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/notes/20200619184615-index.org" "~/notes/notes.org"))
- '(safe-local-variable-values '((eval alex/flowy-note) (olivetti-body-width . 110))))
+ '(org-agenda-files (quote ("~/notes/20200619184615-index.org")))
+ '(safe-local-variable-values
+   (quote
+    ((eval alex/flowy-note)
+     (olivetti-body-width . 110)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
