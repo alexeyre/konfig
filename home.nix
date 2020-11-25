14817963fe600baceaf6d7df91757ce2946ab9ed
Alex Eyre <alex@no-dns-yet.as25178.net>
2020-11-25 09:27:18 +0000

    attempt to make brew persistent

--- home.nix
+++ home.nix
@@ -6,8 +6,20 @@
   programs.home-manager.enable = true;
   home.username = "alex";
   home.homeDirectory = "/Users/alex";
+
+  home.file.brewfile = {
+    source = ./Brewfile;
+    target = ".Brewfile";
+  };
+
   home.packages = with pkgs;
-    [ (haskellPackages.ghcWithPackages (pkgs: [ pkgs.QuickCheck ])) ];
+    [
+      (haskellPackages.ghcWithPackages (pkgs: [ pkgs.QuickCheck ]))
+      lldb
+      exa
+      git
+
+    ];
   programs.tmux = {
     enable = true;
     shortcut = "a";
