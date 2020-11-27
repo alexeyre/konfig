2bc148c12d88a6ed3ae7193991a247bdbf6363f8
Alex Eyre <hi@alexey.re>
2020-11-27 18:47:13 +0000

    rust toolchain nightly

--- home.nix
+++ home.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 {
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      }))
+  (import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz))
+  ];
   imports = [ ./zsh.nix ./emacs ];
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -10,9 +16,16 @@
       exa
       git
       htop
+      latest.rustChannels.nightly.rust
     ];
   programs.tmux = {
     enable = true;
     shortcut = "a";
+    keyMode = "vi";
+  };
+  programs.git = {
+    enable = true;
+    userName = "Alex Eyre";
+    userEmail = "hi@alexey.re";
   };
 }
