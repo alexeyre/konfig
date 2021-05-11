058fa1290ea33cf0e8268a93ce8ef24f4634f0c7
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-11 12:43:22 +0100

    brew: migrate to `lib.str` over `lib.string`

--- modules/brew/default.nix
+++ modules/brew/default.nix
@@ -7,28 +7,28 @@ with lib; {
     description = "Generate a ~/.Brewfile and sync it on generation change";
   };
   options.alex.brew.formulae = mkOption {
-    type = types.listOf types.string;
+    type = types.listOf types.str;
     default = [ ];
     description = "The brew formula commands to pass to the home Brewfile";
   };
   options.alex.brew.casks = mkOption {
-    type = types.listOf types.string;
+    type = types.listOf types.str;
     default = [ ];
     description = "The brew cask commands to pass to the home Brewfile";
   };
   options.alex.brew.mas = mkOption {
-    type = types.listOf types.string;
+    type = types.listOf types.str;
     default = [ ];
     description = "The brew mas commands to pass to the home Brewfile";
   };
   options.alex.brew.taps = mkOption {
-    type = types.listOf types.string;
+    type = types.listOf types.str;
     default =
       [ "homebrew/bundle" "homebrew/cask" "homebrew/core" "homebrew/services" ];
     description = "The brew tap commands to pass to the home Brewfile";
   };
   options.alex.brew.directory = mkOption {
-    type = types.string;
+    type = types.str;
     default = "/opt/homebrew";
     description = "Homebrew install directory";
   };
