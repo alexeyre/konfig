69ed470f0c7cc7a769f72b62661c7d774924e3ca
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-11 12:43:14 +0100

    all: add distributed build entry

--- main.nix
+++ main.nix
@@ -1,4 +1,18 @@
 { pkgs, ... }: {
   nixpkgs.overlays = [ (import ./packages) ];
   nix.package = pkgs.nixUnstable;
+
+  nix.buildMachines = [ {
+	 hostName = "builder";
+	 system = "x86_64-linux";
+	 maxJobs = 1;
+	 speedFactor = 2;
+	 supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+	 mandatoryFeatures = [ ];
+	}] ;
+	nix.distributedBuilds = true;
+	# optional, useful when the builder has a faster internet connection than yours
+	nix.extraOptions = ''
+builders-use-substitutes = true
+	'';
 }
