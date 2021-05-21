bf1f65a842cf140e9ca4aeee85626e5009eb36d4
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-21 21:10:24 +0100

    packages: delete

--- packages/default.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-  ungoogled-chromium-mac =
-    (super.callPackage ./ungoogled-chromium-mac.nix.old { });
-  chromium-dummy = (super.callPackage ./chromium-dummy.nix { });
-}
--- packages/scripts/default.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, lib, config, ... }:
-with lib;
-{
-
-}
