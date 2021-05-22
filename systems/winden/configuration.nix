c040138d3307f8fc9af00b1396bcb7262e72f6fc
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-22 03:10:00 +0100

    winden: use home-manager over config.home-manager

--- systems/winden/configuration.nix
+++ systems/winden/configuration.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... }: {
+{ pkgs, home-manager, ... }: {
   imports = [ ../../os/darwin ];
   networking.hostName = "winden";
   nix.trustedUsers = [ "alex" ];
   services.nix-daemon.enable = false;
 
   # set the main user of the machine!
-  main-user = config.home-manager.users.alex;
+  main-user = home-manager.users.alex;
 }
