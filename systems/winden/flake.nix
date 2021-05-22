4b28b173170d8a63117b241c0a5a8694e236fd19
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-23 00:30:14 +0100

    all: run nixfmt

--- systems/winden/flake.nix
+++ systems/winden/flake.nix
@@ -5,16 +5,13 @@
     darwin.url = "github:lnl7/nix-darwin/master";
     home-manager.url = "github:nix-community/home-manager/master";
 
-
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, darwin, nixpkgs, home-manager }: {
     darwinConfigurations."winden" = darwin.lib.darwinSystem {
-        modules = [
-		(home-manager.darwinModules.home-manager)
-		./configuration.nix
-	];
-      };
+      modules =
+        [ (home-manager.darwinModules.home-manager) ./configuration.nix ];
+    };
   };
 }
