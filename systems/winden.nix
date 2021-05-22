ef2ac422423d28bfe4c13a05d13bf7b9efc7db8f
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-05-22 01:46:37 +0100

    winden: delete old configuration

--- systems/winden.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, ... }: {
-  imports = [ ../os/darwin <home-manager/nix-darwin> ];
-  networking.hostName = "winden";
-  nix.trustedUsers = [ "alex" ];
-  services.nix-daemon.enable = false;
-
-  # set the main user of the machine!
-  main-user = config.home-manager.users.alex;
-}
