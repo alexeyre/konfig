ac5a170cc126249cb8fd209902eb15039e080a1c
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-07-04 23:54:34 +0100

    general: add neovim nightly

--- general/vi/default.nix
+++ general/vi/default.nix
@@ -3,6 +3,7 @@
     home.sessionVariables.EDITOR = "vi";
     programs.neovim = {
       enable = true;
+      package = pkgs.neovim-nightly;
       extraConfig = builtins.readFile ./init.vim;
       viAlias = true;
       vimAlias = true;
--- systems/winden/flake.nix
+++ systems/winden/flake.nix
@@ -5,15 +5,19 @@
     darwin.url = "github:lnl7/nix-darwin/master";
     home-manager.url = "github:nix-community/home-manager/release-21.05";
 
+    # overlays
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, darwin, nixpkgs, home-manager }: {
+  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs: {
     darwinConfigurations."winden" = darwin.lib.darwinSystem {
       inputs = { inherit nixpkgs; };
       modules = [
         home-manager.darwinModules.home-manager
         { home-manager.useGlobalPkgs = true; }
+        { nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlay ]; }
         ./configuration.nix
       ];
     };
