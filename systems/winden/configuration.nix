eb0bb469a4b7954441cc334baac21450be1d344c
Alex Eyre <A.Eyre@sms.ed.ac.uk>
2021-07-11 18:59:53 +0100

    winden: disable x86

--- systems/winden/configuration.nix
+++ systems/winden/configuration.nix
@@ -6,12 +6,16 @@
 
   nix.extraOptions = ''
     system = aarch64-darwin
-    extra-platforms = aarch64-darwin x86_64-darwin
+    # extra-platforms = aarch64-darwin x86_64-darwin
   '';
 
+
   # tmux reattach
-  home-manager.users."${config.main-user}".programs.brew.formulae =
-    [ "fabianishere/personal/pam_reattach" ];
+  home-manager.users."${config.main-user}" = {
+    programs.brew.formulae =
+      [ "fabianishere/personal/pam_reattach" ];
+      home.packages = [ (pkgs.writeScriptBin "rtorrent-tmux" "/usr/bin/arch -arm64 ${pkgs.tmux}/bin/tmux new-session -d -s rtorrent '/usr/bin/arch -arm64 ${pkgs.rtorrent}/bin/rtorrent'") ];
+    };
 
   # set the main user of the machine!
   main-user = "alex";
