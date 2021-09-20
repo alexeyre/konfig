410b8dde6d24abe08ec188864775b7fe766e51ea
Alex Eyre <alexeeyre@gmail.com>
2021-09-20 15:47:54 +0100

    darwin: add things3 shortcut

--- os/darwin/keyboard/default.nix
+++ os/darwin/keyboard/default.nix
@@ -2,6 +2,7 @@
 with lib; {
   home-manager.users."${config.main-user}" = {
     programs.brew.casks = [ "karabiner-elements" ];
+    programs.brew.formulae = [ "yqrashawn/goku/goku" ];
     home.file.gokuConfig = {
       source = ./karabiner.edn;
       target = ".config/karabiner.edn";
