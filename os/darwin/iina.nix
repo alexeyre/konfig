ab5d9a3bd9dc6c21f6fd0c7a4e35d7b5dfc6605c
Alex Eyre <alexeeyre@gmail.com>
2021-12-16 12:50:42 +0000

    darwin/iina: use yt-dlp over youtube-dl

--- os/darwin/iina.nix
+++ os/darwin/iina.nix
@@ -3,6 +3,6 @@ with lib; {
   options.programs.iina.enable = mkEnableOption "Enable the IINA media player";
   config.programs.brew = mkIf config.programs.iina.enable {
     casks = [ "iina" ];
-    formulae = [ "youtube-dl" ];
+    formulae = [ "yt-dlp" ];
   };
 }
