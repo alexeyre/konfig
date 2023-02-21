42e66f8837c83e75ed28311b40acd3a0d5a4bb66
Alex Holder <s2031787@ed.ac.uk>
2023-02-21 22:42:51 +0000

    fish: add plugins

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,13 @@
+direnv hook fish | source
 set -Ux EDITOR nvim
+set -Ux HOMEBREW_NO_AUTO_UPDATE 1
 alias vi nvim
 set -U fish_greeting
 fish_add_path /opt/homebrew/bin
 if status is-interactive
-and not set -q TMUX
-    exec tmux
-end
 fish_vi_key_bindings
 set fish_cursor_default     block      blink
 set fish_cursor_insert      line       blink
 set fish_cursor_replace_one underscore blink
 set fish_cursor_visual      block
-if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
